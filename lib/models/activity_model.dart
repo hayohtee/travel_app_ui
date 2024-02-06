5c209dcb93ad1f14467bfef25de4d3a218c80f6d
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2024-02-06 19:51:46 +0100

    Create activity_model.dart

---
+++ lib/models/activity_model.dart
@@ -0,0 +1,17 @@
+class Activity {
+  Activity({
+    required this.imageUrl,
+    required this.name,
+    required this.type,
+    required this.startTimes,
+    required this.rating,
+    required this.price,
+  });
+
+  String imageUrl;
+  String name;
+  String type;
+  List<String> startTimes;
+  int rating;
+  int price;
+}
